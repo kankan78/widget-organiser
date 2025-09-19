8ade2e116c2f844ee8b3b5206c1b5d68beb9857a
Kankan Das <kksoccer78@gmail.com>
2025-09-19 18:11:01 +0530

    fix added

--- src/app/affilliate/[productId]/page.tsx
+++ src/app/affilliate/[productId]/page.tsx
@@ -1,6 +1,6 @@
 import ProductDetailPage from "../../../component/ProductDetailPage";
 
-export default async function AffilliatePage({ params }) {
-  const { productId } = await params;
-  return <ProductDetailPage productId={productId} />;
+export default async function AffilliateProductPage({ params }: { params: Promise<{ productId: string }> }) {
+	const { productId } = await params;
+	return <ProductDetailPage productId={productId} />;
 }
